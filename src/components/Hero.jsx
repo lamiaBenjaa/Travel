ca6ec24ea527ffd8af21d68d8d1f3d39c86c45e3
mounir ikine <mvner20@gmail.com>
2024-03-13 02:58:00 +0000

    add demo animations to the page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,102 +6,13 @@ import { MdCalendarMonth, MdFormatAlignLeft } from "react-icons/md";
 import { IoPerson } from "react-icons/io5";
 import { IoSearchOutline } from "react-icons/io5";
 import { FaCalendar, FaMapMarkerAlt, FaSearch, FaUser } from "react-icons/fa";
+import {motion} from 'framer-motion'
 
 const Hero = () => {
   const [navActive, setNavActive] = useState("Hostelty");
 
   return (
-    //     <section className="min-h-screen">
-    //       <main className="w-full px-20 relative">
-    //         <video className="w-full h-[84vh] object-cover rounded-xl" src={vid} muted loop autoPlay></video>
-
-    //         <div className="absolute top-32 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-    //           <h1 className="text-6xl text-white font-extrabold mb-4">Explore the whole world and enjoy its beauty</h1>
-    //           <p className="text-white font-semibold">Find and write about your experience around the world</p>
-    //         </div>
-
-    //         <div className="border h-[210px] rounded-lg bg-white w-10/12 mx-auto -translate-y-40 px-10">
-    //   <div className="flex items-center justify-between">
-    //     <div className="flex items-center py-3">
-    //       {["Hostelty", "Flights", "Bus", "Cars"].map((navItem) => (
-    //         <button
-    //           key={navItem}
-    //           onClick={() => setNavActive(navItem)}
-    //           className={`py-2 text-sm px-10 border-b-4 ${navActive === navItem ? 'border-gray-600' : 'border-gray-200'}`}
-    //         >
-    //           {navItem}
-    //         </button>
-    //       ))}
-    //     </div>
-    //     <div className="">
-    //       <Link to="/" className="text-xs text-gray-500">
-    //         Last Searching
-    //       </Link>
-    //     </div>
-    //   </div>
-
-    //   <div className={`flex items-center  py-5 ${navActive === 'Hostelty' ? '' : 'hidden'}`}>
-
-    //   <div className="flex flex-col lg:flex-row items-center gap-6 w-full lg:w-auto">
-    //   <div className="mb-4 lg:mb-0">
-    //     <label htmlFor="destination" className="text-gray-600">Destination</label>
-    //     <div className="flex items-center gap-2 bg-slate-100 px-2 py-3 rounded-md w-full">
-    //       <FaMapMarkerAlt className="text-cyan-700" />
-    //       <select id="destination" className="bg-transparent outline-none border-none text-sm w-full">
-    //         <option value="">Bali, Indonesia</option>
-    //       </select>
-    //     </div>
-    //   </div>
-
-    //   <div className="mb-4 lg:mb-0">
-    //     <label htmlFor="check-in" className="text-gray-600">Check-in</label>
-    //     <div className="flex items-center gap-2 bg-slate-100 px-2 py-3 rounded-md w-full">
-    //       <FaCalendar className="text-cyan-700" />
-    //       <input type="date" id="check-in" className="bg-transparent outline-none border-none text-sm w-full" />
-    //     </div>
-    //   </div>
-
-    //   <div className="mb-4 lg:mb-0">
-    //     <label htmlFor="check-out" className="text-gray-600">Check-out</label>
-    //     <div className="flex items-center gap-2 bg-slate-100 px-2 py-3 rounded-md w-full">
-    //       <FaCalendar className="text-cyan-700" />
-    //       <input type="date" id="check-out" className="bg-transparent outline-none border-none text-sm w-full" />
-    //     </div>
-    //   </div>
-
-    //   <div className="mb-4 lg:mb-0">
-    //     <label htmlFor="room-guest" className="text-gray-600">Room & Guest</label>
-    //     <div className="flex items-center gap-2 bg-slate-100 px-2 py-3 rounded-md w-full">
-    //       <FaUser className="text-cyan-700" />
-    //       <input type="text" id="room-guest" className="bg-transparent outline-none border-none text-sm w-full" />
-    //     </div>
-    //   </div>
-
-    //   <div className="flex items-center pt-5 lg:pt-0">
-    //     <button className="flex items-center justify-center py-1 px-10 rounded-xl text-white bg-cyan-700">
-    //       <FaSearch />
-    //       Search
-    //     </button>
-    //   </div>
-    // </div>
-
-    //   </div>
-    //   <div className={`flex items-center px-8 py-14 ${navActive === 'Flights' ? '' : 'hidden'}`}>
-    //     {/* Flights content */}
-    //     {/* ... */}
-    //   </div>
-    //   <div className={`flex items-center px-8 py-14 ${navActive === 'Bus' ? '' : 'hidden'}`}>
-    //     {/* Bus content */}
-    //     {/* ... */}
-    //   </div>
-    //   <div className={`flex items-center px-8 py-14 ${navActive === 'Cars' ? '' : 'hidden'}`}>
-    //     {/* Cars content */}
-    //     {/* ... */}
-    //   </div>
-    // </div>
-
-    //       </main>
-    //     </section>
+  
     <>
     
     <section className="min-h-screen">
@@ -115,12 +26,29 @@ const Hero = () => {
         ></video>
    
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-extrabold mb-4">
+          <motion.h1 
+           variants={{
+            hidden: { opacity: 0, y: -57 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1, delay: 0.25 }}
+          
+          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-extrabold mb-4">
             Explore the whole world and enjoy its beauty
-          </h1>
-          <p className="text-white font-semibold">
+          </motion.h1>
+          <motion.p 
+           variants={{
+            hidden: { opacity: 0, y: -57 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1, delay: 0.75 }}
+           className="text-white font-semibold">
             Find and write about your experience around the world
-          </p>
+          </motion.p>
         </div>
 
        
@@ -129,8 +57,24 @@ const Hero = () => {
     <section>
       <div className="flex items-center justify-between px-20 pb-10">
         <div className="w-6/12 ">
-          <h1 className="text-6xl font-bold mb-4">Popular Place</h1>
-          <p className="text-lg">Let's enjoy this heaven on earth</p>
+          <motion.h1 
+          variants={{
+            hidden: { opacity: 0, x: -57 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          whileInView="visible"
+          transition={{ duration: 1, delay: 0.25 }}
+           className="text-6xl font-bold mb-4">Popular Place</motion.h1>
+          <motion.p 
+          variants={{
+            hidden: { opacity: 0, x: -57 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          whileInView="visible"
+          transition={{ duration: 1, delay: 0.25 }}
+           className="text-lg">Let's enjoy this heaven on earth</motion.p>
         </div>
         <div className="w-6/12">
           <p className="text-lg ">
