46b9ba8ff77fdd535ab747c5635bd7dfc3d27321
Benjaa <lamiabenlamia528@gmail.com>
2024-03-16 09:27:57 +0000

    animate

--- src/components/Adventure.jsx
+++ src/components/Adventure.jsx
@@ -5,16 +5,38 @@ import seoul from '../components/adventure/seoul.jpg'
 import bali from '../components/adventure/bali.jpg'
 import { PiAirplaneTiltFill } from "react-icons/pi";
 import vedio from '../components/adventure/adventure.mp4'
+import { motion } from 'framer-motion'
 
 export default function Adventure() {
   return (
     <>
         <div className='w-full m-auto lg:pt-28 '>
           <div className='text-center space-y-2 py-8'>
-              <h1 className='text-5xl font-bold'>Let's go on an adventure</h1>
-              <p className='text-lg'>Find and book a great experience</p>
+              <motion.h1
+              variants={{
+                hidden:{opacity:0 , x:-60},
+                visible:{opacity:1 , x:0 , transition:{duration:1 , delay:0.25}},
+              }} 
+              initial='hidden'
+              whileInView='visible'
+               className='text-5xl font-bold'>Let's go on an adventure</motion.h1>
+              <motion.p
+              variants={{
+                hidden:{opacity:0 , x:60},
+                visible:{opacity:1 , x:0 , transition:{duration:1 , delay:0.25}},
+              }} 
+              initial='hidden'
+              whileInView='visible' 
+               className='text-lg'>Find and book a great experience</motion.p>
           </div>
-      <div className='grid grid-cols-2 md:grid-cols-4 gap-8 w-[90%] m-auto md:px-4 lg:px-12 px-12 py-10 relative'>
+      <motion.div
+      variants={{
+        hidden:{opacity:0 , y:200},
+        visible:{opacity:1 , y:0 , transition:{duration:1 , delay:0.4}},
+      }}
+      initial='hidden'
+      whileInView='visible'
+       className='grid grid-cols-2 md:grid-cols-4 gap-8 w-[90%] m-auto md:px-4 lg:px-12 px-12 py-10 relative'>
          <div className='border-dashed border-4 border-zinc-200 opacity-90 w-fit p-2 space-y-2 m-auto'>
               <img className='w-44 m-auto' src={paris} alt='paris' />
               <p className='text-center text-gray-400 font-semibold'>PARIS</p>
@@ -35,12 +57,26 @@ export default function Adventure() {
               <p className='text-center text-gray-400 font-semibold'>BALI</p>
               <p className='absolute top-[50%] left-[53%] md:top-[5%] md:left-[75%] lg:left-[77%]'><PiAirplaneTiltFill className='text-5xl text-white bg-cyan-600 py-2 rounded-full' /></p>
          </div>
-      </div>
+      </motion.div>
     </div>
     <div className='w-full py-12 space-y-10'>
         <div className='flex justify-between w-[80%] m-auto'>
-            <h1 className='text-4xl font-bold'>Book tickets and go now!</h1>
-            <button className=' bg-teal-500 text-gray-100 px-4 drop-shadow-lg py-1 font-bold rounded-lg'>Book now</button>
+            <motion.h1
+             variants={{
+               hidden:{opacity:0 , x:-60},
+               visible:{opacity:1 , x:0 , transition:{duration:1 , delay:0.25}},
+             }} 
+             initial='hidden'
+             whileInView='visible'
+             className='text-4xl font-bold'>Book tickets and go now!</motion.h1>
+            <motion.button
+             variants={{
+               hidden:{opacity:0 , x:60},
+               visible:{opacity:1 , x:0 , transition:{duration:1 , delay:0.25}},
+             }} 
+             initial='hidden'
+             whileInView='visible'
+             className=' bg-teal-500 text-gray-100 px-4 drop-shadow-lg py-1 font-bold rounded-lg'>Book now</motion.button>
         </div>
         <div className='w-[77%] m-auto drop-shadow-md '>
            <video src={vedio} loop autoPlay controls muted className=' rounded-3xl w-full m-auto h-'>
